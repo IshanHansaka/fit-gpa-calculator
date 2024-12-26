75b36fa92bf035256b0950cdbb2d9575cb48182b
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-12-26 22:10:02 +0530

    chore: change header

--- app/component/Header.tsx
+++ app/component/Header.tsx
@@ -1,8 +1,12 @@
 const Header = () => {
   return (
     <header className="text-center mb-4 md:mb-12">
-        <h1 className="text-3xl md:text-4xl font-bold text-blue-600 dark:text-blue-400 mb-2">FIT GPA Calculator</h1>
-      <p className="text-sm md:text-lg text-gray-700 dark:text-gray-300"></p>
+      <div className="text-3xl md:text-4xl font-bold text-blue-600 dark:text-blue-400 mb-2">
+        FIT GPA Calculator
+      </div>
+      <div className="text-sm md:text-lg text-gray-700 dark:text-gray-300">
+
+      </div>
     </header>
   );
 };
