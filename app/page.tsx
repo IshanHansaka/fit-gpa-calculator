968976962011094e97e1e2c5174beb52bf64a656
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-12-26 22:07:39 +0530

    feat: add new components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import Hero from "./component/Hero";
+import Class from "./component/Class";
+import Semester from "./component/Semester";
+import Table from "./component/Table";
+
 export default function Home() {
   return (
-    <></>
+    <>
+      <Hero />
+      <Class />
+      <Semester />
+      <Table />
+    </>
   );
 }
