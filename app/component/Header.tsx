b349bf9fe99232dd5b12cbb90fa23c4a7f1501b2
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-12-26 17:45:15 +0530

    feat: create header component

--- app/component/Header.tsx
+++ app/component/Header.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="text-center mb-4 md:mb-12">
+        <h1 className="text-3xl md:text-4xl font-bold text-blue-600 dark:text-blue-400 mb-2">FIT GPA Calculator</h1>
+      <p className="text-sm md:text-lg text-gray-700 dark:text-gray-300"></p>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
