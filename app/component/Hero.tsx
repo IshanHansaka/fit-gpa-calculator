017015479802dcf1c4bd4a948edf33ddcd646751
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-12-26 17:45:33 +0530

    feat: create hero component

---
+++ app/component/Hero.tsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <div className="text-start mb-4 md:mb-12 px-2 md:px-0">
+        <p className="text-xs md:text-sm text-gray-600 dark:text-gray-400">
+            
+        </p>
+    </div>
+  )
+}
+
+export default Hero
