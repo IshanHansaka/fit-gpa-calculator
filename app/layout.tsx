24e5dc99853558b5d9b8632a6127cb28f0670c8f
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-12-26 17:44:26 +0530

    chore: remove default codes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,37 @@
-import Header from "./component/Header";
+import type { Metadata } from 'next';
+import './globals.css';
 
-export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
+import Header from './component/Header';
+import Footer from './component/Footer';
+
+export const metadata: Metadata = {
+  title: 'FIT-GPA-Calculation',
+  description:
+    'GPA Calculator of Faculty of Information Technology, University of Moratuwa',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <main className="font-work-sans">
-      <Header />
-      {children}
-    </main>
+    <html lang="en" className="h-full">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body className="bg-gradient-to-r from-blue-100 via-purple-100 to-cyan-50 dark:bg-gradient-to-r bg dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 mx-auto pt-10 pb-7">
+        <div className="max-w-7xl mx-auto px-4 md:px-4">
+          <Header />
+          <div className="relative overflow-hidden min-h-screen flex flex-col">
+            <main className="flex-grow container mx-auto px-4 py-6">
+              {children}
+            </main>
+            <Footer />
+          </div>
+        </div>
+      </body>
+    </html>
   );
 }
