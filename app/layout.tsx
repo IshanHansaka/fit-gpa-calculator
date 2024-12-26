d3fe20fd498950a4e5dcc54b1b61469873829d3e
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-12-26 22:07:47 +0530

    feat: add new components

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,12 +24,8 @@ export default function RootLayout({
       <body className="bg-gradient-to-r from-blue-100 via-purple-100 to-cyan-50 dark:bg-gradient-to-r bg dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 mx-auto pt-10 pb-7">
         <div className="max-w-7xl mx-auto px-4 md:px-4">
           <Header />
-          <div className="relative overflow-hidden min-h-screen flex flex-col">
-            <main className="flex-grow container mx-auto px-4 py-6">
-              {children}
-            </main>
-            <Footer />
-          </div>
+          <main>{children}</main>
+          <Footer />
         </div>
       </body>
     </html>
