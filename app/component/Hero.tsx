d343b9d4a52f551e4c42c7b26902be8dfd85f0dd
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-12-26 22:10:34 +0530

    chore: change hero sesction

--- app/component/Hero.tsx
+++ app/component/Hero.tsx
@@ -1,9 +1,9 @@
 const Hero = () => {
   return (
     <div className="text-start mb-4 md:mb-12 px-2 md:px-0">
-        <p className="text-xs md:text-sm text-gray-600 dark:text-gray-400">
-            
-        </p>
+        <div className="text-xs md:text-sm text-gray-600 dark:text-gray-400">
+
+        </div>
     </div>
   )
 }
