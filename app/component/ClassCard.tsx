7f80bcd143c299698c4f59c0c7cfddd064994f4c
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-12-26 22:08:40 +0530

    feat: create new ClassCard component

---
+++ app/component/ClassCard.tsx
@@ -0,0 +1,14 @@
+const ClassCard = () => {
+  return (
+    <div className="bg-blue-100 dark:bg-blue-900 p-4 rounded-lg shadow-sm dark:bg-opacity-20 dark:border-blue-400 backdrop-blur-sm bg-opacity-60 dark:border">
+      <div className="text-lg font-semibold text-blue-600 dark:text-blue-400">
+        Total Credits
+      </div>
+      <div className="text-2xl font-bold text-gray-800 dark:text-gray-200">
+
+      </div>
+    </div>
+  );
+};
+
+export default ClassCard;
