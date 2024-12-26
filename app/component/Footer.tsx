b37fda5344de950781bb79277908f16bcee6e83b
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-12-26 22:09:52 +0530

    chore: change footer

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
-    <footer className="w-full bg-gray-800 text-white py-4">
+    <footer className="mt-8 text-center text-gray-600 dark:text-gray-300 text-sm">
       <div className="container mx-auto px-4 text-center">
-        <p className="text-sm">
-          &copy; {new Date().getFullYear()} Faculty of Information Technology,
-          University of Moratuwa. All rights reserved.
-        </p>
+        <div className="text-sm">
+          &copy; {new Date().getFullYear()} <strong>GPA Calculator</strong>{' '}
+          Faculty of Information Technology, UoM. All rights reserved.
+        </div>
       </div>
     </footer>
   );
