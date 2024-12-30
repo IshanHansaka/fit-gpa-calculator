7c82a59a7111d5ae5ad29d7f91cbb9c11bba7d41
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-12-30 13:33:27 +0530

    feat: add variables and parameters

--- app/component/Class.tsx
+++ app/component/Class.tsx
@@ -3,12 +3,10 @@ import ClassCard from './ClassCard';
 const Class = () => {
   return (
     <div className="space-y-6">
-      <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-3 md:p-6 grid grid-cols-1 md:grid-cols-2 gap-6">
-        <ClassCard />
-        <ClassCard />
-        <div className="col-span-1 md:col-span-2">
-          <ClassCard />
-        </div>
+      <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-3 md:p-6 grid grid-cols-1 md:grid-cols-3 gap-6">
+        <ClassCard title="Total Credits" value={100} />
+        <ClassCard title="Overall GPA" value={1} />
+        <ClassCard title="Class" value="First Class" />
       </div>
     </div>
   );
