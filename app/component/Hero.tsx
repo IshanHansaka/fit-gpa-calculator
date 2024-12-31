1ad1ebeb1513371d6d91f0caddddd415691f7c50
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-12-31 12:17:09 +0530

    feat: add content

--- app/component/Hero.tsx
+++ app/component/Hero.tsx
@@ -1,11 +1,14 @@
 const Hero = () => {
   return (
     <div className="text-start mb-4 md:mb-12 px-2 md:px-0">
-        <div className="text-xs md:text-sm text-gray-600 dark:text-gray-400">
-
-        </div>
+      <div className="text-xs md:text-sm text-gray-600 dark:text-gray-400">
+        Enter your grades to calculate your GPA instantly. Use it to plan how to
+        ace your next semester, aim for that first-class honors , or come to
+        terms with the class that just didn&apos;t go your way. Whatever your GPA
+        journey, we&apos;re here for it!
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
