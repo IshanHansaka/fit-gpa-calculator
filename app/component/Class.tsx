011abb239c8b9cdeedc9e7831e7e17f7b11c0f12
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-12-31 17:10:10 +0530

    chore: change ui format

--- app/component/Class.tsx
+++ app/component/Class.tsx
@@ -6,17 +6,22 @@ interface ClassProps {
   semesterGPA: string;
 }
 
-const Class: React.FC<ClassProps> = ({ totalGPACredits, totalNGPACredits, semesterGPA, }) => {
+const Class: React.FC<ClassProps> = ({ totalGPACredits, totalNGPACredits, semesterGPA }) => {
+  const totalCredits = totalGPACredits + totalNGPACredits;
 
   const overallGPA = semesterGPA;
   return (
     <div className="space-y-6">
       <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-3 md:p-6 grid grid-cols-1 md:grid-cols-3 gap-6">
-        <ClassCard title="Overall GPA" value={overallGPA} description="" />
-        <ClassCard title="Class" value="First Class" description="" />
-          <ClassCard title="Total Credits" value={totalGPACredits} description="" />
-          <ClassCard title="Total Credits" value={totalNGPACredits} description="" />
-          <ClassCard title="Total Credits" value={totalNGPACredits} description="" />
+        <div className="grid grid-cols-1 md:grid-cols-2 md:col-span-3 gap-6">
+          <ClassCard title="Overall GPA" value={overallGPA} description="" />
+          <ClassCard title="Class" value="First Class" description="" />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:col-span-3">
+          <ClassCard title="Total Credits" value={totalCredits} description="" />
+          <ClassCard title="Total GPA Credits" value={totalGPACredits} description="" />
+          <ClassCard title="Total Non GPA Credits" value={totalNGPACredits} description="" />
+        </div>
       </div>
     </div>
   );
