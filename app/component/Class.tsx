dccb78091e1b1aa4418347421c26181993cab7da
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-12-31 16:34:43 +0530

    feat: add total credits and overall gpa

--- app/component/Class.tsx
+++ app/component/Class.tsx
@@ -1,12 +1,22 @@
 import ClassCard from './ClassCard';
 
-const Class = () => {
+interface ClassProps {
+  totalGPACredits: number;
+  totalNGPACredits: number;
+  semesterGPA: string;
+}
+
+const Class: React.FC<ClassProps> = ({ totalGPACredits, totalNGPACredits, semesterGPA, }) => {
+
+  const overallGPA = semesterGPA;
   return (
     <div className="space-y-6">
       <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-3 md:p-6 grid grid-cols-1 md:grid-cols-3 gap-6">
-        <ClassCard title="Total Credits" value={100} description="" />
-        <ClassCard title="Overall GPA" value={1} description="" />
+        <ClassCard title="Overall GPA" value={overallGPA} description="" />
         <ClassCard title="Class" value="First Class" description="" />
+          <ClassCard title="Total Credits" value={totalGPACredits} description="" />
+          <ClassCard title="Total Credits" value={totalNGPACredits} description="" />
+          <ClassCard title="Total Credits" value={totalNGPACredits} description="" />
       </div>
     </div>
   );
