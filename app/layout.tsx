a17e5aea1fb4262f2700fb93c86b1bc82e067e39
Ishan Hansaka Silva <ishanhansakasilva@gmail.com>
2025-02-07 11:41:23 +0530

    feat: optimize seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Theme from "./component/Theme";
 import Arrow from "./component/Arrow";
+import StructuredData from "./StructuredData";
 
 export const metadata: Metadata = {
   title: "FIT GPA Calculator",
@@ -33,7 +34,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning className="h-full">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={metadata.description ?? ""} />
+        <meta name="robots" content="index, follow" />
+        <title>{String(metadata.title) ?? "Default Title"}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className="bg-gradient-to-r from-indigo-100 via-violet-100 to-purple-50 dark:bg-gradient-to-r dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 mx-auto pt-10 pb-7 relative">
+        <StructuredData />
         <Theme />
         <Arrow />
         <div className="max-w-7xl mx-auto px-4 md:px-4">
