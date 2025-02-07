4f0e59b0aab3fa83a5af81aa7c0423ee3dd22d27
Ishan Hansaka Silva <ishanhansakasilva@gmail.com>
2025-02-07 08:50:36 +0530

    feat: add graduation emoji to title

--- app/component/Header.tsx
+++ app/component/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="text-center mb-4 md:mb-12">
       <div className="text-3xl md:text-4xl font-bold text-fuchsia-600 dark:text-fuchsia-400 mb-2">
-        FIT GPA Calculator
+      🎓 FIT GPA Calculator
       </div>
       <div className="text-sm md:text-lg text-gray-700 dark:text-gray-300">
         Calculate your SGPA and OGPA with the FIT grading system
