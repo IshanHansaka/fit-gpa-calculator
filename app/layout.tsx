e9b1e8ce717b615fe2ef69c37ca7dd950f4fa332
Ishan Hansaka Silva <ishanhansakasilva@gmail.com>
2025-02-07 11:26:41 +0530

    feat: optimize seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,20 @@ export const metadata: Metadata = {
   title: "FIT GPA Calculator",
   description:
     "GPA Calculator, Faculty of Information Technology, University of Moratuwa",
+  openGraph: {
+    title: "FIT GPA Calculator",
+    description:
+      "Easily calculate your GPA based on the Faculty of IT, University of Moratuwa's grading system.",
+    url: "https://yourwebsite.com",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "FIT GPA Calculator",
+    description:
+      "Calculate and plan your GPA performance for better academic results.",
+    site: "@yourtwitterhandle",
+  },
 };
 
 export default function RootLayout({
@@ -18,12 +32,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" />
-      </head>
-      <body className="bg-gradient-to-r from-indigo-100 via-violetsky-100 to-purple-50 dark:bg-gradient-to-r bg dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 mx-auto pt-10 pb-7 relative">
+    <html lang="en" suppressHydrationWarning className="h-full">
+      <body className="bg-gradient-to-r from-indigo-100 via-violet-100 to-purple-50 dark:bg-gradient-to-r dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 mx-auto pt-10 pb-7 relative">
         <Theme />
         <Arrow />
         <div className="max-w-7xl mx-auto px-4 md:px-4">
