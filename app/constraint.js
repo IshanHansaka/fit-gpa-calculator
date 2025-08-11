4b4ec4f171bb425541ecd2bf81974501340b7dae
minulck <minulck@gmail.com>
2025-08-11 15:25:06 +0530

    feat: implement PDF download functionality with university logos

---
+++ app/constraint.js
@@ -0,0 +1,7 @@
+// University logo mapping
+export const universityLogos = {
+  'University of Moratuwa': '/UOM.png',
+  'University of Colombo': '/public/Colombo.png',
+  'University of Peradeniya': '/public/Peradeniya.png',
+  // Add more universities and their logo paths here
+};
